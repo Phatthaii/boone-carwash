525640f121f04ea66499f06edade6527ff608fa0
Phatthaii <Khwanruedee6250@gmail.com>
2025-12-20 09:44:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl = "https://rsqhbaimdnmcbaiaxaxe.supabase.co";
+const supabaseKey = "sb_publishable_MCM2rfldeNbAElvO5dHWRg_kErAhrlE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+document.getElementById("bookingForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const car = document.getElementById("car").value;
+
+  const { error } = await supabase
+    .from("bookings")
+    .insert([{ name, car }]);
+
+  if (error) {
+    alert("บันทึกไม่สำเร็จ");
+  } else {
+    alert("จองสำเร็จ");
+  }
+});
 
